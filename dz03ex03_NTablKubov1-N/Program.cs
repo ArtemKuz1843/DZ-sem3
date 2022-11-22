9b9cd23d1716aeb6af2d1b0314ce07f6c8325ecb
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-11-22 17:02:43 +0300

    DZ_3++

--- dz03ex03_NTablKubov1-N/Program.cs
+++ dz03ex03_NTablKubov1-N/Program.cs
@@ -3,16 +3,16 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-Console.WriteLine("введите число");
+Console.WriteLine("Введите число");
 var num = Convert.ToInt32(Console.ReadLine());
 
 Console.Write($"{num} -> ");
 
 for (int i = 1; i <= num; i++)
 {
-        if (i >= 2) // убираем запятую в конце "таблицы"
+        if (i >= 2)
     {
         Console.Write(", ");
     }
-    Console.Write(Math.Pow(i, 3)); // убираем запятую в конце "таблицы", важно, что последнее число именно после запятой, а не до, и цикл не повторяется.
+    Console.Write(Math.Pow(i, 3));
 }
